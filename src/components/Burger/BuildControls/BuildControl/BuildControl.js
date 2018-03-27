8fc74bf3d27d832060971e97849f21e196079c1a
Kenny Drobnack <kennydrobnack@gmail.com>
2018-03-27 06:38:46 -0400

    Fixed typos that were keeping CSS styles from working

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,10 +2,10 @@ import React from 'react'
 import classes from './BuildControl.css'
 
 const buildControl = (props) => (
-    <div className={classes.buildControl}>
-        <div>{props.label}</div>
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Less</button>
-        <button classes={classes.More}>More</button>
+        <button className={classes.More}>More</button>
     </div>
 )
 
