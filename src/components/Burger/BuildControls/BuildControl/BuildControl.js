2622995699682a709396a6daf4e1d05ec1f98b56
Kenny Drobnack <kennydrobnack@gmail.com>
2018-03-27 06:31:57 -0400

    Fixed React error. Now CSS is a bit wonky

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import classes from './BuildControl.css'
 
-const buildControl = (props) => {
+const buildControl = (props) => (
     <div className={classes.buildControl}>
         <div>{props.label}</div>
         <button className={classes.Less}>Less</button>
         <button classes={classes.More}>More</button>
     </div>
-}
+)
 
 export default buildControl
