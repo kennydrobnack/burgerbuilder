d5083e287b4209c0f1803d8a5e0c0dceaa821e40
Kenny Drobnack <kennydrobnack@gmail.com>
2018-04-05 06:44:11 -0400

    Have the basic side drawer almost working sliding in and out when you click on the Menu option

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <div>Backdrop</div>
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-    </Aux>
-)
 
-export default layout
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !this.state.showSideDrawer }
+        })
+    }
+
+    render() {
+
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <div>Backdrop</div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
