929fa3f9bfaeb38839a905de9f559bf4e570ec5a
Kenny Drobnack <kennydrobnack@gmail.com>
2018-04-19 06:42:19 -0400

    Added code to temporarily test ejecting axios interceptors when components they are attached to are unmounted

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -25,6 +25,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
         }
 
         componentWillUnmount() {
+            console.log("Unmounting interceptors: ", this.reqInterceptor, this.resInterceptor)
             axios.interceptors.request.eject(this.reqInterceptor)
             axios.interceptors.response.eject(this.resInterceptor)
         }
