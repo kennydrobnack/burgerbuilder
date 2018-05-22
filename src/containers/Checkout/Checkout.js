a33e5b79a46509f4af0316480d9fbec8ebf2945f
Kenny Drobnack <kennydrobnack@gmail.com>
2018-05-22 07:15:41 -0400

    This is going to be the checkout page

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Checkout extends Component {
+    render() {
+        return (
+            <div>This is going to be the checkout page</div>
+        )
+    }
+}
+
+export default Checkout
