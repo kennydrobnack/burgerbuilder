0e30b7ba6a1c478623d49648d7f0d4c1ba7982e0
Kenny Drobnack <kennydrobnack@gmail.com>
2018-05-30 21:59:42 -0400

    Added React Router to Burger Builder and started wiring it up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
+import { Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -9,7 +10,10 @@ class App extends Component {
       <div>
         This is my app
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
         <Checkout />
       </div>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -84,28 +84,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({ loading: true })
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Kenny',
-                address: {
-                    street: 'Main Street',
-                    zipCode: '12345',
-                    country: 'USA'
-                },
-                email: 'kenny@thisdomaindoesnotexist.com'
-            },
-            deliverymethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false })
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false })
-            })
+        // this.setState({ loading: true })
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Kenny',
+        //         address: {
+        //             street: 'Main Street',
+        //             zipCode: '12345',
+        //             country: 'USA'
+        //         },
+        //         email: 'kenny@thisdomaindoesnotexist.com'
+        //     },
+        //     deliverymethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({ loading: false, purchasing: false })
+        //     })
+        //     .catch(error => {
+        //         this.setState({ loading: false, purchasing: false })
+        //     })
+        this.props.history.push('/checkout')
     }
 
     render() {
