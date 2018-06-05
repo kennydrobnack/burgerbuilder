e40f109dfbace64fb7ddd2292cd43f4b8307685c
Kenny Drobnack <kennydrobnack@gmail.com>
2018-06-04 22:36:09 -0400

    Updated form to have custom input components

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.css'
 import axios from '../../../axios-orders'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
@@ -46,10 +47,10 @@ class ContactData extends Component {
             <div className={classes.ContactData}>
                 <h4>Enter your contact data</h4>
                 <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
-                    <input className={classes.Input} type="email" name="email" placeholder="your@email.com"/>
-                    <input className={classes.Input} type="text" name="street" placeholder="Your Street Address"/>
-                    <input className={classes.Input} type="text" name="postal" placeholder="postal code"/>
+                    <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+                    <Input inputtype="input" type="email" name="email" placeholder="your@email.com" />
+                    <Input inputtype="input" type="text" name="street" placeholder="Your Street Address" />
+                    <Input inputtype="input" type="text" name="postal" placeholder="postal code" />
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
             </div>
