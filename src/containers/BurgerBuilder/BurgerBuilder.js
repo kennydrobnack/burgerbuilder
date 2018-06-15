4e5409ecd6ac66af15ac3864f8992ead95423544
Kenny Drobnack <kennydrobnack@gmail.com>
2018-06-15 06:36:08 -0400

    Updating total price in redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,17 +10,10 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 import { connect } from 'react-redux'
 import * as actionTypes from '../../store/actions'
 
-const INGREDIENT_PRICES = {
-    lettuce: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
-}
+
 
 class BurgerBuilder extends Component {
     state = {
-        totalPrice: 4,
-        purchaseable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -42,54 +35,23 @@ class BurgerBuilder extends Component {
         }).reduce((sum, el) => {
             return sum + el
         }, 0)
-        this.setState({ purchaseable: sum > 0 })
+        return sum > 0
     }
 
     purchaseHandler = () => {
         this.setState({ purchasing: true })
     }
 
-    // addIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type]
-    //     const updatedCount = oldCount + 1
-    //     const updatedIngredients = {
-    //         ...this.state.ingredients
-    //     }
-    //     updatedIngredients[type] = updatedCount
-    //     const priceAddition = INGREDIENT_PRICES[type]
-    //     const oldPrice = this.state.totalPrice
-    //     const newPrice = oldPrice + priceAddition
-    //     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
-    //     this.updatePurchaseableState(updatedIngredients)
-    // }
-
     purchaseCancelHandler = () => {
         this.setState({ purchasing: false })
     }
-
-    // removeIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type]
-    //     if (oldCount <= 0) {
-    //         return
-    //     }
-    //     const updatedCount = oldCount - 1
-    //     const updatedIngredients = {
-    //         ...this.state.ingredients
-    //     }
-    //     updatedIngredients[type] = updatedCount
-    //     const priceAddition = INGREDIENT_PRICES[type]
-    //     const oldPrice = this.state.totalPrice
-    //     const newPrice = oldPrice - priceAddition
-    //     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
-    //     this.updatePurchaseableState(updatedIngredients)
-    // }
 
     purchaseContinueHandler = () => {
         const queryParams = []
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
-        queryParams.push('price=' + this.state.totalPrice)
+        queryParams.push('price=' + this.props.totalPrice)
         const queryString = queryParams.join('&')
         this.props.history.push({
             pathname: '/checkout',
@@ -118,8 +80,8 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        price={this.state.totalPrice}
-                        purchaseable={this.state.purchaseable}
+                        price={this.props.totalPrice}
+                        purchaseable={this.updatePurchaseableState(this.props.ings)}
                         ordered={this.purchaseHandler}
                     />
                 </Aux>
@@ -128,7 +90,7 @@ class BurgerBuilder extends Component {
                 purchaseCancelled={this.purchaseCancelHandler}
                 purchaseContinued={this.purchaseContinueHandler}
                 ingredients={this.props.ings}
-                price={this.state.totalPrice} />
+                price={this.props.totalPrice} />
         }
 
         return (
@@ -144,7 +106,8 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.ingredients,
+        totalPrice: state.totalPrice
     }
 }
 
