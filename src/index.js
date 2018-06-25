3ceea1bf0c668efbdfe7fc02377048d573fa0ac0
Kenny Drobnack <kennydrobnack@gmail.com>
2018-06-24 23:09:16 -0400

    Refactoring how reducers are working

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import reducer from './store/reducer'
+import reducer from './store/reducers/burgerBuilder'
 
 const store = createStore(
     reducer, /* preloadedState, */
